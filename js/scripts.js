c5415c4d440c5eaef930ea74a8e770472c29d9e8
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-03-03 12:55:55 +0100

    add comment

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js file
+
+var name = prompt('Enter your name');
+// welcome text 
+alert('Good morning, ' + name);
+console.log('Good morning, ' + name);
+/*
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+*/
